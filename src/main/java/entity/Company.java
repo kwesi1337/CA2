f1e00ad78e7e1569c06ec2410b4386a442a145bc
josephawwal <josephawwal@192.168.1.17>
2016-10-05 15:50:07 +0200

    Working finally - exception

--- src/main/java/entity/Company.java
+++ src/main/java/entity/Company.java
@@ -20,6 +20,51 @@ public class Company extends InfoEntity  {
 
 
 private String city;
+private String description;
+private String cvr;
+private int numEmployees;
+private int marketValue;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCvr() {
+        return cvr;
+    }
+
+    public void setCvr(String cvr) {
+        this.cvr = cvr;
+    }
+
+    public int getNumEmployees() {
+        return numEmployees;
+    }
+
+    public void setNumEmployees(int numEmployees) {
+        this.numEmployees = numEmployees;
+    }
+
+    public int getMarketValue() {
+        return marketValue;
+    }
+
+    public void setMarketValue(int marketValue) {
+        this.marketValue = marketValue;
+    }
+
 
 
     
